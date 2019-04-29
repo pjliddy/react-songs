e5cafa3838814b2a685e6121313a9cabc56ec56c
pjliddy <pjliddy@gmail.com>
2019-04-29 13:43:26 -0400

    Set up skeleton for App and installed Redux

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div className="ui container" style={{marginTop: '10px', marginBottom: '20px'}}>App</div>
+  )
+}
+
+export default App;
