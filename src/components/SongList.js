81f1d366e388f029331534567c0649d069bcd4d7
pjliddy <pjliddy@gmail.com>
2019-04-29 22:11:48 -0400

    Added event selectSong callback to select buttons

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   // this.props === { songs: state.songs }
@@ -9,7 +10,10 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -30,8 +34,9 @@ class SongList extends Component {
 
 // mapStateToProps is included in every redux component, along with connect
 const mapStateToProps = (state) => {
+  console.log(state);
   return { songs: state.songs };
 }
 
 // connect is a React component that gets a list of songs from the Provider
-export default connect(mapStateToProps) (SongList);
+export default connect(mapStateToProps, { selectSong }) (SongList);
